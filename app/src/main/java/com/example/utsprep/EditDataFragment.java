4c2251b68764709995ba3a45a0e29d3d8ac4b289
Candra Wijaya <candradenis2317@gmail.com>
2024-12-15 18:12:20 +0700

    fixed register, editData now functional, profileFragment now shows data, deleted unused files

--- app/src/main/java/com/example/utsprep/EditDataFragment.java
+++ app/src/main/java/com/example/utsprep/EditDataFragment.java
@@ -42,14 +42,6 @@ public class EditDataFragment extends Fragment {
     private FirebaseAuth auth;
     private FirebaseUser user;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public EditDataFragment() {
         // Required empty public constructor
@@ -63,12 +55,10 @@ public class EditDataFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment EditDataFragment.
      */
-    // TODO: Rename and change types and number of parameters
+
     public static EditDataFragment newInstance(String param1, String param2) {
         EditDataFragment fragment = new EditDataFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -77,8 +67,6 @@ public class EditDataFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
--- app/src/main/java/com/example/utsprep/ProductDetailsFragment.java
+++ app/src/main/java/com/example/utsprep/ProductDetailsFragment.java
@@ -52,15 +52,6 @@ public class ProductDetailsFragment extends Fragment {
     private Button update;
     private int count = 1;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public ProductDetailsFragment() {
         // Required empty public constructor
     }
@@ -73,12 +64,10 @@ public class ProductDetailsFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment ProductDetailsFragment.
      */
-    // TODO: Rename and change types and number of parameters
+
     public static ProductDetailsFragment newInstance(String param1, String param2) {
         ProductDetailsFragment fragment = new ProductDetailsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -87,8 +76,7 @@ public class ProductDetailsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
